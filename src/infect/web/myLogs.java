572c9ddbca9fd11f0e7a17f93b19da01523bfc71
kuiqc <635816623@qq.com>
2020-03-15 00:04:14 +0800

    更新部分函数

--- src/infect/web/myLogs.java
+++ src/infect/web/myLogs.java
@@ -4,9 +4,25 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class myLogs {
+	
+	public String getLatestDate() {
+    	String logPath="D:\\log\\";
+        File file = new File(logPath);
+        File[] tempList = file.listFiles();
+        String fileName = new String();
+        
+        //将所有不晚于date的.log.txt文件的绝对路径添加到files，并判断日期是否超出范围
+        for (int i = 0; i < tempList.length; i++) {
+            if (i==tempList.length-1) {
+                //fileName，不包含路径、后缀（也就是说只有日期yyyy-mm-dd）
+                fileName = tempList[i].getName().substring(0,10);
+            }
+        }
+        return fileName;
+	}
+	
     //读取指定日期以及之前的所有log文件
     public ArrayList<String> getLogsName(String date) {
-    	System.out.print("getLogsName is been used!\n\n\n\n\n\n\n\n\n");
     	String logPath="D:\\log\\";
         ArrayList<String> files = new ArrayList<String>();
         File file = new File(logPath);
